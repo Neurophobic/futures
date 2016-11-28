3db1ed2140ca1f217ddf4f47ab99a12ce5349948
Leo Silverberg <leosilverberg@gmail.com>
2016-11-28 17:14:46 +0000

    formatting

--- map.js
+++ map.js
@@ -2,35 +2,25 @@ var windowW = window.innerWidth;
 var windowH = window.innerHeight;
 var mapW = 2000;
 var mapH = windowH;
-
 var linearCircleR = mapH/5;
 var exponentialCircleR = mapH/2 - 50;
 var centerX = mapW/2;
 var centerY = mapH/2;
-
 var linearBubbleOriginX = centerX + ((linearCircleR) * Math.sin(0));
 var linearBubbleOriginY  = centerY - ((linearCircleR) * Math.cos(0));
 var linearBubbleR = 100;
-
-
 var expoBubbleOriginX = centerX + ((exponentialCircleR) * Math.sin(0));
 var expoBubbleOriginY  = centerY - ((exponentialCircleR) * Math.cos(0));
 var expoBubbleR = 100;
-
 var expoOpen = false;
-
 var t = d3.transition()
     .duration(100);
-
-
- var body = d3.select("#map");
-
-
- var scaler = 0.5;
+var body = d3.select("#map");
+var scaler = 0.5;
 var zoomWidth = (mapW-scaler*mapW)/2
 var zoomHeight = (windowH-scaler*windowH)/2
 
-   var svg = body
+var svg = body
    .append("svg")
    .attr("class","thesvg")
    .attr("width", 2000)
@@ -46,7 +36,7 @@ var zoomHeight = (windowH-scaler*windowH)/2
 
 function toRadians (angle) {
   return angle * (Math.PI / 180);
-}
+};
 
 function getExpoRot(div, place){
     var xRot = centerX + ((exponentialCircleR) * Math.sin(toRadians((360/div)*place)));
@@ -54,7 +44,7 @@ function getExpoRot(div, place){
     var res = xRot +","+ yRot;
 
     return res;
-}
+};
 
 function getLinearRot(div, place){
     var xRot = centerX + ((linearCircleR) * Math.sin(toRadians((360/div)*place)));
@@ -62,51 +52,51 @@ function getLinearRot(div, place){
     var res = xRot +","+ yRot;
 
     return res;
-}
+};
 
 
 
-// var line1 = svg.append("line")          
-//     .style("stroke", "black")  
-//     .attr("x1", centerX)     
-//     .attr("y1", centerY)      
-//     .attr("x2", centerX + ((exponentialCircleR+300) * Math.sin(toRadians(360/8))))     
-//     .attr("y2", centerY - ((exponentialCircleR+300) * Math.cos(toRadians(360/8))));
+var line1 = svg.append("line")          
+    .style("stroke", "black")  
+    .attr("x1", centerX)     
+    .attr("y1", centerY)      
+    .attr("x2", centerX + ((exponentialCircleR+300) * Math.sin(toRadians(360/8))))     
+    .attr("y2", centerY - ((exponentialCircleR+300) * Math.cos(toRadians(360/8))));
 
-// var line2 = svg.append("line")          
-//     .style("stroke", "black")  
-//     .attr("x1", centerX)     
-//     .attr("y1", centerY)      
-//     .attr("x2", centerX + ((exponentialCircleR+300) * Math.sin(toRadians((360/8)*3))))     
-//     .attr("y2", centerY - ((exponentialCircleR+300) * Math.cos(toRadians((360/8)*3))));
+var line2 = svg.append("line")          
+    .style("stroke", "black")  
+    .attr("x1", centerX)     
+    .attr("y1", centerY)      
+    .attr("x2", centerX + ((exponentialCircleR+300) * Math.sin(toRadians((360/8)*3))))     
+    .attr("y2", centerY - ((exponentialCircleR+300) * Math.cos(toRadians((360/8)*3))));
 
-// var line3 = svg.append("line")          
-//     .style("stroke", "black")  
-//     .attr("x1", centerX)     
-//     .attr("y1", centerY)      
-//     .attr("x2", centerX + ((exponentialCircleR+300) * Math.sin(toRadians((360/8)*5))))     
-//     .attr("y2", centerY - ((exponentialCircleR+300) * Math.cos(toRadians((360/8)*5))));
+var line3 = svg.append("line")          
+    .style("stroke", "black")  
+    .attr("x1", centerX)     
+    .attr("y1", centerY)      
+    .attr("x2", centerX + ((exponentialCircleR+300) * Math.sin(toRadians((360/8)*5))))     
+    .attr("y2", centerY - ((exponentialCircleR+300) * Math.cos(toRadians((360/8)*5))));
 
-// var line5 = svg.append("line")          
-//     .style("stroke", "black")  
-//     .attr("x1", centerX)     
-//     .attr("y1", centerY)      
-//     .attr("x2", centerX + ((exponentialCircleR+300) * Math.sin(toRadians((360/8)*7))))     
-//     .attr("y2", centerY - ((exponentialCircleR+300) * Math.cos(toRadians((360/8)*7))));   
+var line5 = svg.append("line")          
+    .style("stroke", "black")  
+    .attr("x1", centerX)     
+    .attr("y1", centerY)      
+    .attr("x2", centerX + ((exponentialCircleR+300) * Math.sin(toRadians((360/8)*7))))     
+    .attr("y2", centerY - ((exponentialCircleR+300) * Math.cos(toRadians((360/8)*7))));   
 
-// var centerCircle = svg.append("circle").attr({
-//     cx:centerX,
-//     cy:centerY,
-//     r:linearCircleR/2,
-//     fill:"white"
-// })
+var centerCircle = svg.append("circle").attr({
+    cx:centerX,
+    cy:centerY,
+    r:linearCircleR/2,
+    fill:"white"
+});
 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //exponential
-
-
-
-
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var exponentialCircle = svg.append("circle").attr({
     cx: centerX,
@@ -383,9 +373,13 @@ expoGroup8.append("text")
 
 
 
-
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //linear
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 var linearCircle = svg.append("circle").attr({
     cx: centerX,
     cy: centerY,
@@ -472,6 +466,17 @@ linearGroup1.transition(t).attr("transform", "translate("+getLinearRot(4, 1)+")"
 linearGroup2.transition(t).attr("transform", "translate("+getLinearRot(4, 2)+")");
 linearGroup3.transition(t).attr("transform", "translate("+getLinearRot(4, 3)+")");
 linearGroup4.transition(t).attr("transform", "translate("+getLinearRot(4, 4)+")");
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//info
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 
 var expoInfoLocked = false;
@@ -571,14 +576,7 @@ expoInfoGroup1.append("foreignObject")
     .append("xhtml:div") 
     .attr("class","expoInfoText")
     .html('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla euismod consequat. Nam rutrum rutrum enim. Cras lacinia dolor id augue sagittis, in faucibus lectus mattis. Morbi vitae nisi nulla. Proin fermentum massa at tellus interdum cursus. Cras ullamcorper egestas dolor, nec euismod massa sollicitudin sed. Duis vitae pulvinar metus. Integer vehicula egestas enim, a mollis odio ultricies in.');
-// var expoInfoText1 = expoInfoGroup1
-// .append("text")
-// .attr("class","expoInfoText")
-// .attr("x", -exponentialCircleR/1.5)
-// .attr("y", -exponentialCircleR/1.5+50)
-// .text(function(){
-//     return "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla euismod consequat. Nam rutrum rutrum enim. Cras lacinia dolor id augue sagittis, in faucibus lectus mattis. Morbi vitae nisi nulla. Proin fermentum massa at tellus interdum cursus. Cras ullamcorper egestas dolor, nec euismod massa sollicitudin sed. Duis vitae pulvinar metus. Integer vehicula egestas enim, a mollis odio ultricies in.";
-// });
+
 
 var expoClip2 = svg.append("g")
     .attr("class", "circle-container")
